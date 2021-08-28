68ea42cb28a3210f193120d0611eab85a436a031
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-08-29 02:41:31 +0530

    password checker added

--- static/js/signup.js
+++ static/js/signup.js
@@ -7,3 +7,19 @@ function typeCheck() {
         uId.placeholder = "Teacher Name";
     }
 }
+
+let passCheck = () => {
+    let password = document.getElementById('password').value;
+    let conPass = document.getElementById('conPassword').value;
+    let btn = document.getElementById('submitBtn');
+    let passMsg = document.getElementById('passMsg');
+    if (password == conPass) {
+        btn.disabled = false;
+        passMsg.style.display = "none";
+    } else {
+        btn.disabled = true;
+        passMsg.style.display = "block";
+    }
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'not matching';
+}
