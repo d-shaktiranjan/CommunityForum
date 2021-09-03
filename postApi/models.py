1c1de5651bb4db6630eb8de202172f6b3f530278
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-09-03 12:36:12 +0530

    Student DB created

--- postApi/models.py
+++ postApi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class StudentData(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=25, unique=True)
+    roll = models.CharField(max_length=10, unique=True)
+    branch = models.CharField(max_length=10)
+    dateOfJoin = models.DateField()
