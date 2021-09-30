dbedcbf8c5dbeb930017c22ddfbc59a5f5727205
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-09-30 21:00:04 +0530

    DB added

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from home.models import Students, Teachers
 # Register your models here.
+admin.site.register(Students)
+admin.site.register(Teachers)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=20)
+    regNumber = models.IntegerField(primary_key=True)
+    password = models.TextField()
+    branch = models.CharField(max_length=10)
+    dateTimeOfJoin = models.DateTimeField(default=datetime.now())
+
+
+class Teachers(models.Model):
+    name = models.CharField(max_length=20)
+    mailId = models.CharField(max_length=25, primary_key=True)
+    password = models.TextField()
+    branch = models.CharField(max_length=10)
+    dateTimeOfJoin = models.DateTimeField(default=datetime.now())
