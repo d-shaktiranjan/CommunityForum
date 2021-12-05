2683301c3a13e2416b120d9c495a278ead2afc28
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-12-06 01:25:03 +0530

    show post count & list in profile

--- home/views.py
+++ home/views.py
@@ -85,6 +85,11 @@ def profile(request):
     if request.session.get("log"):
         user = Students.objects.filter(sID=request.session.get("sId")).first()
         myDict = {"user": user}
+        questions = Quentions.objects.filter(
+            uID=request.session.get("sId")).all()
+        myDict["totalQuestions"] = len(questions)
+        if len(questions) > 0:
+            myDict["questionList"] = questions
         return render(request, "profile.html", myDict)
     return redirect(index)
 
