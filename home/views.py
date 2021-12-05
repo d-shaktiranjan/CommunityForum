abe0d5853a6df3a3fe5d6e294e32dfbb3f3c7b61
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-12-06 00:18:31 +0530

    alert added

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from home.models import Students, Teachers, Quentions
 from django.contrib.auth.hashers import make_password, check_password
 from datetime import datetime
@@ -26,12 +26,12 @@ def signup(request):
                 newStudent = Students(
                     name=name, regNumber=regNumber, password=make_password(password+generateSalt(uId)), branch="CSE", dateTimeOfJoin=datetime.now())
                 newStudent.save()
-                return HttpResponse(f"{name} is a student added")
+                return alert(request, True, "Your account is created", "Now you can login", "/")
             newTeacher = Teachers(name=name, mailId=uId,
                                   password=make_password(password+generateSalt(uId)), branch="CSE", dateTimeOfJoin=datetime.now())
             newTeacher.save()
-            return HttpResponse(f"{name} is a Teacher added")
-        return HttpResponse("Pas & con not match")
+            return alert(request, True, "Your account is created", "Now you can login", "/")
+        return alert(request, False, "Error", "Password not confirm password not matched.", "/")
     return redirect(index)
 
 
@@ -41,12 +41,12 @@ def userLogin(request, isStudent, uId, password):
     else:
         user = Teachers.objects.filter(mailId=uId).first()
     if user is None:
-        return HttpResponse("Please Signup")
+        return alert(request, False, "Signup first", "Create an account to continue", "/")
     if check_password(password, user.password):
         request.session['log'] = True
         request.session['uId'] = uId
         return redirect(index)
-    return HttpResponse("Password not matched")
+    return alert(request, False, "Password not matched", "", "/")
 
 
 def login(request):
@@ -73,5 +73,15 @@ def postQuestion(request):
         new = Quentions(title=title, about=about,
                         dateTimeOfPost=datetime.now())
         new.save()
-        return HttpResponse("Post added")
+        return alert(request, True, "Post added", "Wait for others response", "/")
     return render(request, "postQuestion.html")
+
+
+def alert(request, isSuccess, msg, about, link):
+    myDict = {
+        "msg": msg,
+        "about": about,
+        "link": link,
+        "status": "success" if isSuccess else "error"
+    }
+    return render(request, "alert.html", myDict)
