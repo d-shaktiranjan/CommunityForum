d61edf9ebb86e209806052e43c5fdc4837ef40a1
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-12-09 23:22:41 +0530

    us OS module to import static

--- major_project/settings.py
+++ major_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +122,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    os.path.join(BASE_DIR, 'static'),
+    # '/var/www/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
