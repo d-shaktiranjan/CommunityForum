85840fc12745b49aa47e4155ccc73c52220b3c7b
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-12-10 11:43:36 +0530

    give reaction def added

--- home/reaction.py
+++ home/reaction.py
@@ -1,7 +1,7 @@
-from home.models import Students, Teachers
+from home.models import *
 
 
-def isReacted(isStudent, userID, isQuestion, contentID):
+def getRecord(isStudent, userID, isQuestion):
     if isStudent:
         user = Students.objects.filter(sID=userID).first()
     else:
@@ -11,6 +11,31 @@ def isReacted(isStudent, userID, isQuestion, contentID):
         records = user.questionRecord
     else:
         records = user.answerRecord
+    return records
+
+
+def isReacted(isStudent, userID, isQuestion, contentID):
+    records = getRecord(isStudent, userID, isQuestion, contentID)
     if contentID in records.keys():
         return True
     return False
+
+
+def giveReaction(isStudent, userID, isQuestion, contentID, isLiked):
+    if isReacted(isStudent, userID, isQuestion, contentID):
+        return False
+    try:
+        records = getRecord(isStudent, userID, isQuestion, contentID)
+        if isQuestion:
+            about = Quentions.objects.filter(qID=contentID).first()
+        else:
+            about = Answers.objects.filter(aID=contentID).first()
+        if isLiked:
+            records[contentID] = True
+            about.likeCount += 1
+        else:
+            records[contentID] = False
+            about.disLikeCount += 1
+        return True
+    except:
+        return False
