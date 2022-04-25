6ed37d1344a05841ed400fda55f542df9f8a92ea
Rahul Choudhury <52797278+rahul-choudhury@users.noreply.github.com>
2022-04-26 00:18:22 +0530

    fixed postQuestion UI and added categories

--- static/js/postQuestion.js
+++ static/js/postQuestion.js
@@ -1,9 +1,16 @@
 let isImage = document.getElementById("isImage");
 let imageInput = document.getElementById("imageInput");
+let submitButton = document.getElementById("submitButton");
 isImage.addEventListener("click", () => {
     if (isImage.checked == true) {
-        imageInput.innerHTML = `<input type="file" name="postImage" accept="image/*" id="postImage" required />`;
+        imageInput.innerHTML = `<input type="file" name="postImage" accept="image/*" id="postImage" required />
+        <label for="postImage">
+        <i class="fa-solid fa-upload"></i>
+        Browse
+        </label>`;
+        submitButton.classList.add("post-submit-margin");
     } else {
         imageInput.innerHTML = "";
+        submitButton.classList.remove("post-submit-margin");
     }
 })
